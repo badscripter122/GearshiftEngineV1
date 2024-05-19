36252e73c7a6e65a586da0384a8c3883a3a9c047
badscripter <youpoop63@gmail.com>
2024-05-19 11:00:22 -0400

    more stuff to console

--- GearshiftEngineV0/Main.cpp
+++ GearshiftEngineV0/Main.cpp
@@ -3,5 +3,33 @@
 
 int main() {
 	std::cout << "  #####  #######    #    ######   #####  #     # ### ####### #######    ####### #     #  #####  ### #     # ####### \n#     # #         # #   #     # #     # #     #  #  #          #       #       ##    # #     #  #  ##    # #  \n#       #        #   #  #     # #       #     #  #  #          #       #       # #   # #        #  # #   # #      \n#  #### #####   #     # ######   #####  #######  #  #####      #       #####   #  #  # #  ####  #  #  #  # ##### \n#     # #       ####### #   #         # #     #  #  #          #       #       #   # # #     #  #  #   # # #    \n#     # #       #     # #    #  #     # #     #  #  #          #       #       #    ## #     #  #  #    ## #     \n #####  ####### #     # #     #  #####  #     # ### #          #       ####### #     #  #####  ### #     # #######\n\n\n\n\n";
-	std::cout << "LOADING WINDOW\n";
+	std::cout << "LOADING WINDOW\n\n\n";
+	int glfw = glfwInit();
+
+	if (glfw == 0) {
+		std::cout << "GLFW INIT FAILED\n\n\n";
+		return -1;
+	}
+	else if(glfw == 1){
+		std::cout << "GLFW INIT DONE\n\n\n";
+	}
+	GLFWwindow* window = glfwCreateWindow(720, 820,"Gearshift Engine", nullptr, nullptr);
+
+	std::cout << "Window loop starting...\n\n\n\n";
+
+	
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+
+	std::cout << "DESTROYING WINDOW\n\n";
+	glfwDestroyWindow(window);
+	std::cout << "TERMINATE GLFW\n\n";
+	glfwTerminate();
+
+	std::cout << "PRESS ENTER TO CONTINE\n\n\n";
+
+	std::cin.get();
 }
